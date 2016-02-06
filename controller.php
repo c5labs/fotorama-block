6ad8cd629451f5574ee5ecce51e04314a559e906
Oliver Green <green2go@gmail.com>
2016-02-06 08:35:18 +0000

    Version bump.

--- controller.php
+++ controller.php
@@ -14,7 +14,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'concrete-fotorama-block';
     protected $appVersionRequired = '5.7.1';
-    protected $pkgVersion = '0.10.1';
+    protected $pkgVersion = '0.10.2';
 
     public function getPackageName() 
     {
